ac8ef1f6037e95d179ed2a5ea9c1332e5bc3f22d
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-07-28 15:14:21 +0200

    (useEffect & lifecycle) display list of characters + button to delete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const baseURL = 'https://ih-crud-api.herokuapp.com';
+
+  const [characters, setCharacters] = useState(null);
+
+  useEffect( () => {
+    fetchCharacters();
+  }, []);
+
+
+  const fetchCharacters = () => {
+    axios
+      .get(baseURL + '/characters')
+      .then((response) => {
+        const allCharacters = response.data;
+        const fistTen = allCharacters.slice(0, 10)
+        setCharacters(fistTen);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+
+  const deleteCharacter = (id) => {
+    axios
+      .delete(baseURL + '/characters/' + id)
+      .then((response) => {
+        fetchCharacters();
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  
+  const renderCharacters = () => {
+    const result = characters.map(element => {
+      return (
+        <div className='character'>
+          <h2>{element.name}</h2>
+          <p>Occupation: {element.occupation}</p>
+          <p>Weapon: {element.weapon}</p>
+
+          <button onClick={ () => {deleteCharacter(element.id) }}>Delete</button>
+        </div>
+      );
+    });
+
+    return result;
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>React Charates App</h1>
       </header>
+
+      <div>
+        { characters === null
+          ? <p>loading...</p>
+          : renderCharacters()
+        }
+      </div>
+
     </div>
   );
 }
